d337b67d9e66c76b5fab3795a61eca583c4764d4
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-11 13:25:04 +0200

    example adaptive navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+import Navbar from "./components/Navbar";
+import './components/Navbar.css';
+
+function App() {
+    return ( <
+        Navbar / >
+    );
+}
+
+export default App;
